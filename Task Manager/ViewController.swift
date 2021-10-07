d9a96f9ec3d11402de06646e7f89a86f37ee90eb
duilan <soyduilan@gmail.com>
2021-10-06 19:09:46 -0500

    primer commit y gitignore

---
+++ Task Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task Manager
+//
+//  Created by Duilan on 06/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
