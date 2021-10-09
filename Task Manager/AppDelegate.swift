851dc7bf732678081ad1b336b1ea81ab7bc2d002
duilan <soyduilan@gmail.com>
2021-10-08 20:51:29 -0500

    agrega apariencia basica para el navbar

--- Task Manager/AppDelegate.swift
+++ Task Manager/AppDelegate.swift
@@ -9,28 +9,39 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Configuracion de la apariencia del navbar
+        configureNavigationBarAppearance()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func configureNavigationBarAppearance() {
+        // init an appearance
+        let appearance = UINavigationBarAppearance()
+        
+        // Remove the border and shadow
+        appearance.configureWithTransparentBackground()
+        
+        // Transparent Background Color
+        appearance.backgroundColor = .clear
+        
+        // Button Items Color
+        UINavigationBar.appearance().tintColor = .label
+        
+        // Text & Color for titles
+        appearance.titleTextAttributes = [ .foregroundColor: UIColor.label ]
+        
+        // Text & Color for LargeTitles
+        appearance.largeTitleTextAttributes = [ .foregroundColor: UIColor.label ]
+        
+        // Apply the appearance
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    
 }
 
