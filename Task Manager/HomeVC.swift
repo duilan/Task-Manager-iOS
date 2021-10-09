97d68507c0a601ee58b570ee50b667aef4824f0f
duilan <soyduilan@gmail.com>
2021-10-08 20:19:35 -0500

    Configuracion inicial sin storyboards

---
+++ Task Manager/HomeVC.swift
@@ -0,0 +1,21 @@
+//
+//  HomeVC.swift
+//  Task Manager
+//
+//  Created by Duilan on 08/10/21.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    func setup() {        
+        view.backgroundColor = .systemBackground
+    }
+    
+}
