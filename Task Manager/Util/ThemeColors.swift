babb60c1c92424d2d5b3790b8642209474117cb6
duilan <soyduilan@gmail.com>
2021-10-12 21:23:51 -0500

    agrega utilidad para utilizar set de colores como variables

---
+++ Task Manager/Util/ThemeColors.swift
@@ -0,0 +1,32 @@
+//
+//  ThemeColors.swift
+//  Task Manager
+//
+//  Created by Duilan on 12/10/21.
+//
+
+import UIKit
+
+struct ThemeColors {
+    static let backgroundPrimary = UIColor(named: "BackgroundPrimary") ?? .systemBackground
+    
+    // Title && Text Colors
+    static let title = UIColor(named: "Title") ?? .gray
+    static let titleInverted = UIColor(named: "TitleInverted") ?? .gray
+    
+    static let subtitle = UIColor(named: "Subtitle") ?? .gray
+    
+    static let text = UIColor(named: "Text") ?? .gray
+    static let textInverted = UIColor(named: "TextInverted") ?? .gray
+    static let textDisabled = UIColor(named: "TextDisabled") ?? .gray
+    
+    static let navbar = UIColor(named: "Navbar") ?? .gray
+    
+    static let green = UIColor(named: "Green") ?? .gray
+    static let blueA = UIColor(named: "GradientBlueColorA") ?? .gray
+    static let blueB = UIColor(named: "GradientBlueColorB") ?? .gray
+    
+    static let accentColor = UIColor(named: "AccentColor") ?? .gray
+    
+}
+
