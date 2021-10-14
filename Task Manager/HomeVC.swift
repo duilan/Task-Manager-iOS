a0758fe630776e4f0146f0b58661ffb4aae30ec5
duilan <soyduilan@gmail.com>
2021-10-13 22:48:33 -0500

    agrega botones de usuario y añadir proyecto en inicio

--- Task Manager/HomeVC.swift
+++ Task Manager/HomeVC.swift
@@ -12,10 +12,19 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        setupNavigationButtonItems()
     }
     
-    func setup() {        
-        view.backgroundColor = .systemBackground
+    func setup() {
+        view.backgroundColor = ThemeColors.backgroundPrimary
+        title = "Proyectos"
+    }
+    
+    func setupNavigationButtonItems() {
+        let userImageSymbol = UIImage(systemName: "person.circle.fill")
+        let addImageSymbol = UIImage(systemName: "plus")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: userImageSymbol, style: .plain, target: self, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: addImageSymbol, style: .plain, target: self, action: nil)
     }
     
 }
