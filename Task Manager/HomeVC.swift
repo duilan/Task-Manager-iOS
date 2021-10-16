c78f2b18d7c314f17b79a771b6c4308fd49a2d2c
duilan <soyduilan@gmail.com>
2021-10-15 23:24:37 -0500

    agrega scrollview & contentView al inicio

--- Task Manager/HomeVC.swift
+++ Task Manager/HomeVC.swift
@@ -9,22 +9,55 @@ import UIKit
 
 class HomeVC: UIViewController {
     
+    private let scrollView = UIScrollView()
+    private let contentView = UIView(frame: .zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
         setupNavigationButtonItems()
+        setupScrollView()
+        setupContentView()
     }
     
-    func setup() {
+    private func setup() {
         view.backgroundColor = ThemeColors.backgroundPrimary
         title = "Proyectos"
     }
     
-    func setupNavigationButtonItems() {
+    private func setupNavigationButtonItems() {
         let userImageSymbol = UIImage(systemName: "person.circle.fill")
         let addImageSymbol = UIImage(systemName: "plus")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: userImageSymbol, style: .plain, target: self, action: nil)
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: addImageSymbol, style: .plain, target: self, action: nil)
+    }
+    
+    private func setupScrollView() {
+        view.addSubview(scrollView)
+        scrollView.showsVerticalScrollIndicator = false
+        //scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        // Constraints
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
+        ])
+    }
+    
+    private func setupContentView() {
+        scrollView.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        let paddingForContentView: CGFloat = 16
+        // Constraints
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -paddingForContentView * 2),
+            contentView.heightAnchor.constraint(greaterThanOrEqualTo: scrollView.heightAnchor, constant: 1)
+        ])
     }
     
 }
