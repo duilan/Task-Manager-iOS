85fe9a5794d249222e9893343147bb80d3a7153c
duilan <soyduilan@gmail.com>
2022-07-06 00:50:48 -0500

    agrega scrolling al ColorPicker

--- Task Manager/CustomViews/FormElements/TMColorPickerView.swift
+++ Task Manager/CustomViews/FormElements/TMColorPickerView.swift
@@ -9,23 +9,24 @@ import UIKit
 
 class TMColorPickerView: UIView {
     
-    private let stackH = UIStackView()
+    private let titleLabel = UILabel()
+    private let scrollView = UIScrollView()
+    private let HStack = UIStackView()
     
     private(set) var indexColor: Int = 0 {
         didSet {
-            self.selectColorOption()
+            self.showColorSelected()
         }
     }
     
-    var defaultHeight: CGFloat = 40
-    
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: .zero, height: defaultHeight)
-    }
+    private let colorTopBottomPadding: CGFloat = 20
+    private let colorHeight: CGFloat = 35
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
-        setup()
+        setupTitleLabel()
+        setupScrollView()
+        setupHStack()
         configureColorOptions()
     }
     
@@ -33,46 +34,81 @@ class TMColorPickerView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setup() {
-        addSubview(stackH)
-        stackH.axis = .horizontal
-        stackH.alignment = .center
-        stackH.distribution = .fillEqually
-        stackH.spacing = 20
-        stackH.translatesAutoresizingMaskIntoConstraints = false
+    private func setupScrollView() {
+        addSubview(scrollView)
+        self.clipsToBounds = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsHorizontalScrollIndicator = false
         
         NSLayoutConstraint.activate([
-            stackH.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            stackH.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            scrollView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            scrollView.heightAnchor.constraint(equalToConstant: colorHeight + colorTopBottomPadding)
+        ])
+    }
+    
+    private func setupHStack() {
+        scrollView.addSubview(HStack)
+        HStack.axis = .horizontal
+        HStack.alignment = .center
+        HStack.distribution = .fillEqually
+        HStack.spacing = 20
+        HStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            HStack.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+            HStack.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 15),
+            HStack.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -15),
+            HStack.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+        ])
+    }
+    
+    private func setupTitleLabel() {
+        addSubview(titleLabel)
+        titleLabel.text = "Color".uppercased()
+        titleLabel.textColor = ThemeColors.title.withAlphaComponent(0.6)
+        titleLabel.font = .systemFont(ofSize: 12, weight: .medium)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.heightAnchor.constraint(equalToConstant: 15),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 6),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8)
         ])
     }
     
     private func configureColorOptions() {
         // crea lista de colores existentes eb el model
         ProjectColors.allCases.forEach { (color) in
-            let btnOption = UIButton(frame: .zero)
-            stackH.addArrangedSubview(btnOption)
+            let btnOption = UIButton()
+            HStack.addArrangedSubview(btnOption)
             btnOption.backgroundColor = color.value // uicolor
             btnOption.tintColor = .white
             btnOption.layer.opacity = 0.3
-            btnOption.tag = color.rawValue // int que se almacenara en coredata
+            btnOption.tag = color.rawValue // int, sera indexColor para indetificar el color
             
             btnOption.clipsToBounds = true
-            btnOption.layer.cornerRadius = defaultHeight/2
+            btnOption.layer.cornerRadius = colorHeight / 2
             btnOption.translatesAutoresizingMaskIntoConstraints = false
-            btnOption.heightAnchor.constraint(equalToConstant: defaultHeight).isActive = true
-            btnOption.widthAnchor.constraint(equalToConstant: defaultHeight).isActive = true
+            btnOption.widthAnchor.constraint(equalToConstant: colorHeight).isActive = true
+            btnOption.heightAnchor.constraint(equalTo: btnOption.widthAnchor).isActive = true
             //tap action
             btnOption.addTarget(self, action: #selector(colorTapped(_:)), for: .touchUpInside)
         }
-        
         //set default color selected
         indexColor = 0
     }
     
-    private func selectColorOption() {
+    @objc private func colorTapped(_ btn: UIButton) {
+        indexColor = btn.tag
+    }
+    
+    private func showColorSelected() {
         
-        guard let btnOptions = stackH.arrangedSubviews as? [UIButton] else { return }
+        guard let btnOptions = HStack.arrangedSubviews as? [UIButton] else { return }
         
         // remove checkmark image of all options
         btnOptions.forEach { (btn) in
@@ -88,10 +124,6 @@ class TMColorPickerView: UIView {
             btnOptions[self.indexColor].layer.opacity = 1
             btnOptions[self.indexColor].transform = .init(scaleX: 1.1, y: 1.1)
         }
-    }
-    
-    @objc private func colorTapped(_ btn: UIButton) {
-        indexColor = btn.tag
     }
     
 }
