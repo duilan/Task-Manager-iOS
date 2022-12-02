0bc31009c5560f1c5f7553895377bab3cffa1f9c
duilan <soyduilan@gmail.com>
2022-12-02 00:36:56 -0600

    agrega metodo de eliminacion al VM de Projects

--- Task Manager/CustomViews/ChildViewControllers/ProjectListViewModel.swift
+++ Task Manager/CustomViews/ChildViewControllers/ProjectListViewModel.swift
@@ -6,9 +6,22 @@
 //
 
 protocol ProjectListDelegate: AnyObject {
+    func didDeletedProject(_ project: Project)
 }
 
 class ProjectListViewModel {
+    
     private let repository: ProjectRepository = ProjectRepository()
     weak var delegate: ProjectListDelegate?
+    
+    func delete(project: Project) {
+        let result = repository.delete(project)
+        switch result {
+        case .success():
+            delegate?.didDeletedProject(project)
+        case .failure(let error):
+            print(error)
+        }
+    }
+    
 }
